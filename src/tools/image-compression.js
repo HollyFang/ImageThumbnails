f566c78a5453752a253de334bce6567ea4e2774f
HollyFang <hollyfan@foxmail.com>
2018-03-23 14:30:39 +0800

    copy src image to dist

--- src/tools/image-compression.js
+++ src/tools/image-compression.js
@@ -1,22 +1,33 @@
 const images = require("images");
-const fs=require('fs');
+const fs = require('fs');
 
-let path=__dirname+'/../photoes';
+let path = __dirname + '/../photoes';
 //console.log(path);
-let files=fs.readdirSync(path);
-        files.forEach((val,index) => {
-        	if(!val.startsWith('.')){
-            let fPath=path+'/'+val;
-            let stats=fs.statSync(fPath);
-            if(stats.isFile()) {
-            	compress(val,path+'/',__dirname+"/../../dist/images/");
+let files = fs.readdirSync(path);
+files.forEach((val, index) => {
+    if (!val.startsWith('.')) {
+        let fPath = path + '/' + val;
+        let stats = fs.statSync(fPath);
+        if (stats.isFile()) {
+            let destDirect = __dirname + "/../../dist/thumbnails/";
+            if (!fs.existsSync(destDirect)) {
+                fs.mkdirSync(destDirect);
             }
-          }
+            compress(val, path + '/', destDirect);
+            destDirect = __dirname + "/../../dist/images/";
+            if (!fs.existsSync(destDirect)) {
+                fs.mkdirSync(destDirect);
+            }
+            fs.createReadStream(path + '/' + val).pipe(fs.createWriteStream(destDirect + val));
+
+        }
+    }
+});
+
+function compress(srcName, srcDirect, destDirect) {
+    images(srcDirect + srcName)
+        .size(200)
+        .save(destDirect + srcName, {
+            quality: 20
         });
-function compress(srcName,srcDirect,destDirect){
-	images(srcDirect+srcName) 
-    .size(300)
-    .save(destDirect+srcName, {
-        quality : 50 
-    });
 }
